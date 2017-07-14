858365b9fcb930731f58e8aedeffa04d128f3631
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-07-13 21:09:05 -0300

    Main page layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,35 +7,117 @@
 
         <title>{E}schedule - An App for organized peoples</title>
         <link rel="stylesheet" href="css/app.css">
+
+
+        <!--  Material Dashboard CSS    -->
+        <link href="css/material-dashboard.css" rel="stylesheet"/>
+
+        <!--     Fonts and icons     -->
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <nav class="navbar navbar-inverse navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="#">{E}schedule</a>
+   
+        
+            <div class="sidebar" data-color="green" data-image="../assets/img/sidebar-1.jpg">
+                <!--
+                    Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+                    Tip 2: you can also add an image using data-image tag
+                -->
+
+                <div class="logo">
+                    <a href="http://www.creative-tim.com" class="simple-text">
+                        {E}schedule
+                    </a>
                 </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Configurações</a></li>
+                <div class="sidebar-wrapper">
+                    <ul class="nav">
+                        <li class="active">
+                            <a href="dashboard.html">
+                                <i class="material-icons">dashboard</i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
                     </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Sair</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
+                </div>
             </div>
-        </nav>
 
-        <div class="container">
-            @yield('content')
+        <div class="main-panel">
+            <nav class="navbar navbar-transparent navbar-absolute">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">@yield('title')</a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="material-icons">dashboard</i>
+                                    <p class="hidden-lg hidden-md">Dashboard</p>
+                                </a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="material-icons">notifications</i>
+                                    <span class="notification">5</span>
+                                    <p class="hidden-lg hidden-md">Notifications</p>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Mike John responded to your email</a></li>
+                                    <li><a href="#">You have 5 new tasks</a></li>
+                                    <li><a href="#">You're now friend with Andrew</a></li>
+                                    <li><a href="#">Another Notification</a></li>
+                                    <li><a href="#">Another One</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
+                                   <i class="material-icons">person</i>
+                                   <p class="hidden-lg hidden-md">Profile</p>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="form-group  is-empty">
+                                <input type="text" class="form-control" placeholder="Buscar...">
+                                <span class="material-input"></span>
+                            </div>
+                            <button type="submit" class="btn btn-white btn-round btn-just-icon">
+                                <i class="material-icons">search</i><div class="ripple-container"></div>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="content">
+                <div class="container-fluid">
+                    @yield('content')                    
+                </div>
+            </div>
         </div>
     </body>
     <script src="js/app.js" charset="utf-8"></script>
+        <!--   Core JS Files   -->
+    <script src="js/jquery-3.1.0.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/material.min.js" type="text/javascript"></script>
+
+    <!--  Charts Plugin -->
+    <script src="js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="js/bootstrap-notify.js"></script>
+
+    <!-- Material Dashboard javascript methods -->
+    <script src="js/material-dashboard.js"></script>
 </html>
