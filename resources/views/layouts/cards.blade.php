ddfae69da3258399dd6d63cf5b08d82851744e3e
PCoelho07 <pedro_coelho_pv@hotmail.com>
2017-07-17 21:42:49 -0300

    Initial layout: almost there

---
+++ resources/views/layouts/cards.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+
+@section('cards')
+	 <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-header" data-background-color="orange">
+                    <i class="material-icons">content_copy</i>
+                </div>
+                <div class="card-content">
+                    <p class="category">Disciplinas cursadas</p>
+                    <h3 class="title">49/50</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-header" data-background-color="blue">
+                    <i class="material-icons">store</i>
+                </div>
+                <div class="card-content">
+                    <p class="category">Média de aproveitamento por semestre</p>
+                    <h3 class="title">$34,245</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card card-stats">
+                <div class="card-header" data-background-color="red">
+                    <i class="material-icons">info_outline</i>
+                </div>
+                <div class="card-content">
+                    <p class="category">Carga horária complementar</p>
+                    <h3 class="title">75 <small>hrs</small></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
